2a08f195270b40254797aa2fe569b5ecef09a548
Etchu Ferdinand <ferdinandetchu@gmail.com>
2025-05-27 10:16:05 +0000

    add padding left and right on the top navigation

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -49,7 +49,7 @@ export function Navbar() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 max-w-screen-2xl items-center justify-between">
+      <div className="container px-6 flex h-16 max-w-screen-2xl items-center justify-between">
         <Logo />
         
         <nav className="hidden md:flex items-center space-x-6 text-sm font-medium">
